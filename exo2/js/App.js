54fe30a0720f53cc34928496805a679ceb175d2f
salutsalut <anais.p.04@hotmail.com>
2024-10-15 17:57:23 +0200

    EXO_2_MOVEMENT

--- exo2/js/App.js
+++ exo2/js/App.js
@@ -4,13 +4,17 @@ export default class App {
   constructor() {
     this.canvas;
     this.ctx;
-    // créer le canevas
     this.createCanvas();
-    // créer une lettre
-    this.letter = new Letter(100, 100, 100);
-    // initialiser interaction click
+    // Positionne cercle au centre de la fenêtre
+    const initialX = this.width / 2;
+    const initialY = this.height / 2;
+    this.letter = new Letter(initialX, initialY, 130);
+    this.targetX = this.letter.x;
+    this.targetY = this.letter.y;
+    this.speed = 10;
+    // suivis du cercle à la souris
+    this.isFollowing = true;
     this.initInteraction();
-    // dessiner canvas
     this.draw();
   }
 
@@ -23,31 +27,72 @@ export default class App {
     this.canvas.height = height;
     document.body.appendChild(this.canvas);
 
-    this.ctx.fillStyle = "#ff92b1";
-    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
+    // this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     document.body.appendChild(this.canvas);
   }
 
   initInteraction() {
-    document.addEventListener(
-      "click",
-      // "mousemoove" "mouseup" "mousedown" "mousedrag" autre mouse option
-
-      (evenement) => {
-        this.letter.x = evenement.x;
-        this.letter.y = evenement.y;
+    // ACTIIVE le suivis du cercle à la souris
+    document.addEventListener("mousemove", (evenement) => {
+      if (this.isFollowing) {
+        this.targetX = evenement.x;
+        this.targetY = evenement.y;
       }
-    );
+    });
+
+    document.addEventListener("click", (evenement) => {
+      // DESACTIVER le suivis du cercle à la souris
+      this.isFollowing = !this.isFollowing;
+      // Si le suivi est activé, enregistrer la nouvelle cible
+      if (this.isFollowing) {
+        this.targetX = evenement.x;
+        this.targetY = evenement.y;
+      }
+    });
   }
 
+  // ANCIEN CODE POUR autres ref
+  // initInteraction() {
+  //   document.addEventListener(
+  //     "click",
+  //     // "mousemoove" "mouseup" "mousedown" "mousedrag" autre mouse option
+
+  //     (evenement) => {
+  //       this.letter.x = evenement.x;
+  //       this.letter.y = evenement.y;
+  //     }
+  //   );
+  // }
+  //   this.letter.x += 1;
+  //   this.letter.y += 1;
+  //   // transformer le canevas en flip book
+  //   requestAnimationFrame(this.draw.bind(this));
+  // }
+
   draw() {
-    this.ctx.clearRect(0, 0, this.width, this.height);
+    // Enlèvement du clear -> il effaçait les anciennes images
+    // this.ctx.clearRect(0, 0, this.width, this.height);
+
     // dessiner cercle
     this.letter.dessine(this.ctx);
-    this.letter.x += 1;
-    this.letter.y += 1;
-    // transformer le canevas en flip book
+    // CALCULER distance entre la position actuelle et la cible
+    const dx = this.targetX - this.letter.x;
+    const dy = this.targetY - this.letter.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    // DEPLACEMENT à LA CIBLE ( c'est en pixel) ( 1-3 fait tremnler le cercle )
+    if (distance > 5) {
+      // Normaliser le vecteur de direction
+      // (direction et distance )
+      const normalizedDx = dx / distance;
+      const normalizedDy = dy / distance;
+
+      // Mettre à jour la position de la lettre + vitesse
+      this.letter.x += normalizedDx * this.speed;
+      this.letter.y += normalizedDy * this.speed;
+    }
+
+    // Transformer le canevas en flipbook (image)
     requestAnimationFrame(this.draw.bind(this));
   }
 }

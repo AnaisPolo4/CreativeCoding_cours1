e6e7caf0c9456f7546a3741337aa01a108cda16d
Anais Polo <anais.polo@ecal.ch>
2024-10-15 17:56:15 +0200

    Add files via upload

--- exo2/js/Letter.js
+++ exo2/js/Letter.js
@@ -3,18 +3,24 @@ export default class Letter {
     this.x = x;
     this.y = y;
     this.radius = radius;
-    this.character = "A";
-    this.fontSize = 32;
+    this.character = "allo?";
+    this.fontSize = 80;
+    this.color = "#00FF00";
+    this.strokeStyle = "#00FF00";
+    this.lineWidth = 3;
   }
 
   dessine(ctx) {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.strokeStyle = this.strokeStyle;
     ctx.stroke();
-    // dessiner la lettre au centre du cercle
+    ctx.lineWidth = this.lineWidth;
     ctx.font = `${this.fontSize}px Helvetica`;
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
+    // Définir la couleur du texte
+    ctx.fillStyle = this.color;
     ctx.fillText(this.character, this.x, this.y);
   }
 }
